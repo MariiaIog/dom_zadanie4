fb298a20094e6fb6a34f997daccb863b00abfa30
MariiaIog <Suhaya_me@mail.ru>
2022-11-29 02:53:43 +0300

    Задача 29

---
+++ task29/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int[] GettingArray(int length)
+{
+    int[] array = new int[length];
+    Random rd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rd.Next(0, 10);
+    }
+    return array;
+}
+
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = GettingArray(8);
+printArray(array);
